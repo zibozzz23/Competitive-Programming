5100d2dfd855a28e064b28f363f01277b7dbbaaf
Mung Jirapinya <j.dumsuk@gmail.com>
2026-09-12 15:19:31 +0700

    Create bubblesort2.cpp

---
+++ Sorting/bubblesort2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[105];
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < n-1; ++i){
+        for(int j = 0; j < n-i-1; ++j){
+            if(a[j] > a[j+1]){
+                swap(a[j], a[j+1]);
+            }
+        }
+    }
+
+    for(int i = 0; i < n; ++i){
+        cout << a[i] << ' ';
+    }
+}
